aae8895305fbd0180738cdcbaa2ecce26a7e48b5
s2300283 <s2300283@edubc.fi>
2023-11-21 22:43:31 +0200

    task2.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,5 @@
 import React from 'react';
 
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} - {part.exercises} exercises
-    </p>
-  );
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-}
-
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  );
-}
-
-const Course = ({ course }) => {
-  const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0);
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <p>Total of {totalExercises} exercises</p>
-    </div>
-  );
-}
-
 const App = () => {
   const course = {
     name: 'Half Stack application development',
@@ -59,9 +23,20 @@ const App = () => {
     ]
   }
 
+  const totalExercises = course.parts.reduce((total, part) => {
+    console.log('what is happening',total,part)
+    return total + part.exercises
+  }, 0);
+
   return (
     <div>
-      <Course course={course} />
+      <h1>{course.name}</h1>
+      {course.parts.map(part => (
+        <p key={part.id}>
+          {part.name} - {part.exercises} exercises
+        </p>
+      ))}
+      <p>Total of {totalExercises} exercises</p>
     </div>
   )
 }
